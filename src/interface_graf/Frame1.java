9d1b2f950266ae499640661c3c44740c0eedfd17
PattLD <118557101+PattLD@users.noreply.github.com>
2024-10-28 21:10:13 -0300

    posicionando campos no cadastro + adicionando icon

--- src/interface_graf/Frame1.java
+++ src/interface_graf/Frame1.java
@@ -6,12 +6,13 @@ import javax.swing.*;  // Pacote para os componentes da GUI (JFrame, JButton, JL
 import javax.swing.border.TitledBorder;
 import java.awt.*;     // Pacote para layouts e manipulação de gráficos
 import java.awt.event.*;  // Pacote para manipulação de eventos (ActionListener)
+import java.net.URL;
 
 import static interface_graf.Style.*;
 
 public class Frame1 extends JFrame {
     
-	private Paciente paciente;
+	public static Paciente paciente;
 
     int numLabel = 30;
     int numPanel = 10;
@@ -26,11 +27,11 @@ public class Frame1 extends JFrame {
 	private double peso;
 	private double altura;
 
+    private String[] genderStrings = { "SELECIONE", "Feminino", "Masculino" };
+    private String[] exercisetrings = { "SELECIONE", "Sedentário", "Levemente ativo", "Moderamente ativo", "Muito ativo" };
 
 
-
-    
-	public Frame1(){
+    public Frame1(){
 
         this.setTitle("NutriPlan");
         this.setSize(700, 650);
@@ -39,9 +40,8 @@ public class Frame1 extends JFrame {
         this.setLayout(new BorderLayout());
         this.setLocationRelativeTo(null);
 
-        ////////////////////////
-        //BORDAS
-
+        ImageIcon icon = new ImageIcon("src/resouce/icon.png");
+        this.setIconImage(icon.getImage());
 
         ////////////////////////
         //PAINEIS
@@ -74,43 +74,48 @@ public class Frame1 extends JFrame {
 
 
 
-        // PAINEL IMPUT
-        JPanel imputPanel = new JPanel();
-        imputPanel.setBackground(verdeClaro);
-        imputPanel.setBorder(loweredbevel);
-        //imputPanel.setPreferredSize(new Dimension(400, 330));
+        // PAINEL FORMS
+        JPanel formsPanel = new JPanel();
+        formsPanel.setBackground(verdeClaro);
+        formsPanel.setBorder(loweredbevel);
         //layout
-        imputPanel.setLayout(new GridBagLayout());
-        GridBagConstraints gbcImput = new GridBagConstraints();
-        gbcImput.insets = new Insets(1, 5, 1, 5);
-        gbcImput.weightx = 1;
-        gbcImput.gridwidth = 1;
-        gbcImput.gridheight = 1;
-        gbcImput.fill = GridBagConstraints.BOTH; // Permite preencher horizontalmente
+        formsPanel.setLayout(new GridBagLayout());
+        formsPanel.setBorder(BorderFactory.createCompoundBorder(
+                loweredbevel,
+                BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+        GridBagConstraints gbcForms = new GridBagConstraints();
+        gbcForms.insets = new Insets(1, 5, 1, 5);
+        gbcForms.gridwidth = 1;
+        gbcForms.gridheight = 1;
+        gbcForms.fill = GridBagConstraints.BOTH; // Permite preencher horizontalmente
+        gbcForms.anchor = GridBagConstraints.WEST;
 
         // PAINEL BOTAO
         JPanel backPane = new JPanel();
         backPane.setBackground(transparente);
-        backPane.setLayout(new FlowLayout());
+        backPane.setLayout(new BorderLayout());
 
 
 
         ////////////////////////
         //LABELS
+
+        //IMAGEM
+        ImageIcon imagemLogo = getimagemLogo();
+        JLabel labelLogo = new JLabel(imagemLogo);
+
+        // Adiciona o JLabel ao frame
+
+
+
+
+        //LABEL - CADASTRO
         JLabel label[] = new JLabel[numLabel];
         for (int i = 0; i < numLabel; i++) {
             label[i] = new JLabel();
             label[i].setFont(FONT_REGULAR_12);
             label[i].setHorizontalAlignment(SwingConstants.LEFT);
         }
-
-        ImageIcon imagemLogo = getimagemLogo();
-
-        // Adiciona a imagem ao JLabel
-        JLabel labelLogo = new JLabel(imagemLogo);
-
-        // Adiciona o JLabel ao frame
-        logoPanel.add(labelLogo);
 
         label[0].setText("Cadastro de Pacientes");
         label[0].setForeground(Color.WHITE);  // Cor do texto
@@ -120,62 +125,22 @@ public class Frame1 extends JFrame {
         label[0].setBackground(Style.verdeEscuro);
         label[0].setBorder(loweredbevel);
 
-        //LABEL - DADOS
-        //titulos
-        Font boldFont = FONT_BOLD_12;
+        label[1].setText("Nome completo");
+        label[2].setText("CPF");
+        label[3].setText("Sexo");
+        label[4].setText("Data de Nascimento");
+        label[5].setText("Altura");
+        label[6].setText("Peso");
+        label[7].setText("Nivel de atividade");
+        label[8].setText("Objetivo:");
 
-        TitledBorder[] title = new TitledBorder[numLabel];
-        for (int i = 0; i < numLabel; i++) {
-            title[i] = BorderFactory.createTitledBorder(etched, "");
-            title[i].setTitleFont(boldFont);
-        }
-        //renomeando
-        title[0].setTitle("Nome");
-        title[1].setTitle("Sexo");
-        title[2].setTitle("Idade");
-        title[3].setTitle("IMC");
-        title[4].setTitle("Peso");
-        title[5].setTitle("Altura");
-        title[6].setTitle("Objetivo");
-        title[7].setTitle("Nivel de atividade");
-        title[8].setTitle("Taxa Metabólica Basal (TMB)");
-        title[9].setTitle("Gasto Energético Total (GET)");
-        //labels
-        for(int i=0;i<10;i++) {
-            int j = i+1;
-            label[j].setBorder(title[i]);
-        }
-        label[1].setText("Nome");
-        label[2].setText("Sexo");
-        label[3].setText("Idade");
-        label[4].setText("IMC");
-        label[5].setText("Peso");
-        label[6].setText("Altura:");
-        label[7].setText("Objetivo:");
-        label[8].setText("Nivel de atividade:");
-        label[9].setText("Taxa Metabólica Basal (TMB):");
-        label[10].setText("Gasto Energético Total (GET):");
+        JComboBox gender = new JComboBox(genderStrings);
+        gender.setSelectedIndex(2);
+        gender.setSelectedIndex(0);
 
-        label[11].setText("REGULAR");
-
-
-        //LABELS - IMPUT
-        label[12].setText("ALIMENTO");
-        label[12].setFont(FONT_BOLD_12);
-        label[12].setHorizontalAlignment(SwingConstants.CENTER);
-        label[12].setBorder(raisedbevel);
-
-        label[13].setText("PLANO");
-        label[13].setFont(FONT_BOLD_12);
-        label[13].setHorizontalAlignment(SwingConstants.CENTER);
-        label[13].setBorder(raisedbevel);
-
-        label[14].setText("Nome");
-        label[15].setText("Calorias adicionadas");
-        label[16].setText("Quantidade (g)");
-        label[17].setText("Calorias restantes");
-        label[18].setText("kcal/100g");
-        label[19].setText("Calorias necessárias");
+        JComboBox exerciseFrequency = new JComboBox(exercisetrings);
+        exerciseFrequency.setSelectedIndex(4);
+        exerciseFrequency.setSelectedIndex(0);
 
         ////////////////////////
         // BOTOES
@@ -184,10 +149,8 @@ public class Frame1 extends JFrame {
             button[i] = new JButton();
             //button[i].setPreferredSize(new Dimension(150, 20));
         }
-        button[0].setText("Adicionar");
-        button[1].setText("Remover");
-        button[2].setText("Atualizar");
-        button[3].setText("Voltar");
+        button[0].setText("Voltar");
+        button[1].setText("Cadastro");
 
         ////////////////////////
         // CAIXA DE TEXTO
@@ -197,16 +160,20 @@ public class Frame1 extends JFrame {
             //txtdook[i].setPreferredSize(new Dimension(50, 30));
         }
 
-        txtdook[1].setEditable(false);
-        txtdook[1].setEditable(false);
+        JTextArea txtObjetivo = new JTextArea();
+        txtObjetivo.setLineWrap(true); // Quebra de linha automática
+        txtObjetivo.setWrapStyleWord(true); // Quebra por palavras
 
-        txtdook[3].setEditable(false);
-        txtdook[3].setEditable(false);
+        // Ajusta a fonte para manter um estilo similar ao JTextField
+        txtObjetivo.setFont(new Font("Arial", Font.PLAIN, 12));
 
-        txtdook[5].setEditable(false);
-        txtdook[5].setEditable(false);
+        // Adiciona uma borda com aparência de JTextField
+        txtObjetivo.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(Color.GRAY, 1), // Borda externa cinza
+                BorderFactory.createEmptyBorder(5, 5, 5, 5)
+        ));
 
-        ////////////////////////
+                ////////////////////////
         // GRID DO PAINEL CENTRAL
 
         gbcCentral.weightx = 1;
@@ -230,7 +197,7 @@ public class Frame1 extends JFrame {
         gbcCentral.gridy = 2;
         gbcCentral.weighty = 0.95;
         gbcCentral.insets = new Insets(0, 5, 5, 5);
-        mainPanel.add(imputPanel, gbcCentral);
+        mainPanel.add(formsPanel, gbcCentral);
 
         gbcCentral.insets = new Insets(5, 5, 5, 5);
 
@@ -241,15 +208,133 @@ public class Frame1 extends JFrame {
         mainPanel.add(backPane, gbcCentral);
 
         ////////////////////////
+        //ADD LOGO
+        logoPanel.add(labelLogo);
+
+        ////////////////////////
         // GRID DO PAINEL DADOS
 
-        backPane.add(button[3], FlowLayout.LEFT);
-        button[3].addActionListener(new ActionListener() {
+
+        //nome
+        gbcForms.gridx = 0;
+        gbcForms.gridy = 0;
+        gbcForms.gridwidth = 3;
+        gbcForms.insets = new Insets(5, 5, 2, 5);
+        formsPanel.add(label[1], gbcForms);
+        //
+        gbcForms.gridx = 0;
+        gbcForms.gridy = 1;
+        gbcForms.gridwidth = 3;
+        gbcForms.insets = new Insets(2, 5, 5, 5);
+        formsPanel.add(txtdook[0], gbcForms);
+
+        //cpf
+        gbcForms.gridx = 0;
+        gbcForms.gridy = 2;
+        gbcForms.gridwidth = 1;
+        gbcForms.weightx = 0.40;
+        gbcForms.insets = new Insets(5, 5, 2, 5);
+        formsPanel.add(label[2], gbcForms);
+        //
+        gbcForms.gridx = 0;
+        gbcForms.gridy = 3;
+        gbcForms.gridwidth = 1;
+        gbcForms.insets = new Insets(2, 5, 5, 5);
+        formsPanel.add(txtdook[1], gbcForms);
+
+        //sexo
+        gbcForms.gridx = 1;
+        gbcForms.gridy = 2;
+        gbcForms.gridwidth = 1;
+        gbcForms.weightx = 0.30;
+        gbcForms.insets = new Insets(5, 5, 2, 5);
+        formsPanel.add(label[3], gbcForms);
+        //
+        gbcForms.gridx = 1;
+        gbcForms.gridy = 3;
+        gbcForms.gridwidth = 1;
+        gbcForms.insets = new Insets(2, 5, 5, 5);
+        formsPanel.add(gender, gbcForms);
+
+        //data de nascimento
+        gbcForms.gridx = 2;
+        gbcForms.gridy = 2;
+        gbcForms.gridwidth = 1;
+        gbcForms.weightx = 0.20;
+        gbcForms.insets = new Insets(5, 5, 2, 5);
+        formsPanel.add(label[4], gbcForms);
+        //
+        gbcForms.gridx = 2;
+        gbcForms.gridy = 3;
+        gbcForms.gridwidth = 1;
+        gbcForms.insets = new Insets(2, 5, 5, 5);
+        formsPanel.add(txtdook[2], gbcForms);
+
+        //altura
+        gbcForms.gridx = 0;
+        gbcForms.gridy = 4;
+        gbcForms.gridwidth = 1;
+        gbcForms.weightx = 0.40;
+        gbcForms.insets = new Insets(5, 5, 2, 5);
+        formsPanel.add(label[5], gbcForms);
+        //
+        gbcForms.gridx = 0;
+        gbcForms.gridy = 5;
+        gbcForms.gridwidth = 1;
+        gbcForms.insets = new Insets(2, 5, 5, 5);
+        formsPanel.add(txtdook[3], gbcForms);
+
+        //peso
+        gbcForms.gridx = 1;
+        gbcForms.gridy = 4;
+        gbcForms.gridwidth = 1;
+        gbcForms.weightx = 0.30;
+        gbcForms.insets = new Insets(5, 5, 2, 5);
+        formsPanel.add(label[6], gbcForms);
+        //
+        gbcForms.gridx = 1;
+        gbcForms.gridy = 5;
+        gbcForms.gridwidth = 1;
+        gbcForms.insets = new Insets(2, 5, 5, 5);
+        formsPanel.add(txtdook[4], gbcForms);
+
+        //nivel de atividade
+        gbcForms.gridx = 2;
+        gbcForms.gridy = 4;
+        gbcForms.gridwidth = 1;
+        gbcForms.weightx = 0.20;
+        gbcForms.insets = new Insets(5, 5, 2, 5);
+        formsPanel.add(label[7], gbcForms);
+        //
+        gbcForms.gridx = 2;
+        gbcForms.gridy = 5;
+        gbcForms.gridwidth = 1;
+        gbcForms.insets = new Insets(2, 5, 5, 5);
+        formsPanel.add(exerciseFrequency, gbcForms);
+
+        //objetivo
+        gbcForms.gridx = 0;
+        gbcForms.gridy = 6;
+        gbcForms.gridwidth = 3;
+        gbcForms.weightx = 1;
+        gbcForms.insets = new Insets(5, 5, 2, 5);
+        formsPanel.add(label[8], gbcForms);
+        //
+        gbcForms.gridx = 0;
+        gbcForms.gridy = 7;
+        gbcForms.gridwidth = 3;
+        gbcForms.weighty = 0.5;
+        gbcForms.insets = new Insets(2, 5, 5, 5);
+        formsPanel.add(txtObjetivo, gbcForms);
+
+
+        backPane.add(button[0], BorderLayout.LINE_START);
+        backPane.add(button[1], BorderLayout.LINE_END);
+
+        button[1].addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
 
-                Frame2 frame2 = new Frame2();
-                frame2.setVisible(true);
-                Frame1.this.setVisible(false);
+                abrirFrame2();
 
             }
         });
@@ -259,6 +344,12 @@ public class Frame1 extends JFrame {
         this.setVisible(true);
 		
 	}
+
+    public void abrirFrame2(){
+        Frame2 frame2 = new Frame2();
+        frame2.setVisible(true);
+        Frame1.this.setVisible(false);
+    }
 	
 	public Paciente getPaciente() {
 	    return paciente;
--- src/interface_graf/Frame2.java
+++ src/interface_graf/Frame2.java
@@ -4,15 +4,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
+import javax.swing.*;
 import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 
@@ -38,6 +30,9 @@ public class Frame2 extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
         this.setLocationRelativeTo(null);
+
+        ImageIcon icon = new ImageIcon("src/resouce/icon.png");
+        this.setIconImage(icon.getImage());
 
         ////////////////////////
         //PAINEIS
